d194d09ac6a3762e4d64e5d15d2d83cdb0925d5b
Mounica Chirva <mounica.chirva@kingsmensoftware.com>
2016-10-25 17:02:48 -0400

    Initial server, database, ejs communication for items list screen

---
+++ schema.js
@@ -0,0 +1,19 @@
+var Schema = {
+  items: {
+    ItemId: {type: 'string', maxlength: 254, nullable: false, primary: true},
+    ItemName: {type: 'string', maxlength: 50, nullable: false},
+    Description: {type: 'string', maxlength: 500, nullable: false},
+    Price: {type: 'decimal', nullable: false},
+    OnSale: {type: 'boolean', nullable: false},
+    Discount: {type: 'decimal', nullable: true}
+  },
+  categories: {
+    CategoryId: {type: 'string', maxlength: 254, nullable: false, primary: true},
+    CategoryName: {type: 'string', maxlength: 50, nullable: false}
+  },
+  itemCategory: {
+    CategoryId: {type: 'string', maxlength: 254, nullable: false},
+    ItemId: {type: 'string', maxlength: 254, nullable: false}
+  }
+};
+module.exports = Schema;
---
+++ server.js
@@ -0,0 +1,77 @@
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'Catawba',
+        charset  : 'utf8'
+  }
+});
+
+var express = require("express");
+var Bookshelf = require('bookshelf')(knex);
+var bodyParser = require('body-parser');
+var jwt    = require('jsonwebtoken');
+
+var app = express();
+var router = express.Router();
+
+var JWTKEY = 'CatawbaDatabase'; // Key for Json Web Token
+
+// body-parser middleware for handling request variables
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+var Item = Bookshelf.Model.extend({
+    tableName: 'items'
+});
+
+var Category = Bookshelf.Model.extend({
+  tableName: 'categories'
+});
+
+
+var CategoryItem = Bookshelf.Model.extend({
+  tableName: 'itemCategory',
+    category: function() {
+        return this.belongsTo(Category, "CategoryId");
+    },
+    item: function() {
+        return this.belongsTo(Item, "ItemId");
+    }
+});
+
+var Items = Bookshelf.Collection.extend({
+  model: Item
+});
+
+var Categories = Bookshelf.Collection.extend({
+  model: Category
+});
+
+app.post('/', function(req, res) {
+    var decoded = jwt.verify(req.body.token, JWTKEY);
+      if(decoded) {
+        knex.from('items')
+            .then(function (collection) {
+                res.render('pages/index', {error: false, collection});
+            })
+            .catch(function (err) {
+                res.status(500).json({error: true, data: {message: err.message}});
+            });
+          }else {
+            res.json({error: true, data: {message: 'invalid token'}});
+          }
+});
+
+
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
